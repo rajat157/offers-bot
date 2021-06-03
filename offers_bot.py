1ab6de49a2cccfeec88b00b6547ef733a6a7e7fb
Rajat R Kumar <rajat157@gmail.com>
2021-06-03 16:45:16 +0530

    Rewrote bot code

--- offers_bot.py
+++ offers_bot.py
@@ -3,6 +3,44 @@ from dotenv import load_dotenv
 import discord
 import os
 from time import sleep
+import logging
+from signal import signal, SIGINT, SIGTERM
 
 load_dotenv()
 
+logging.basicConfig(level=logging.INFO)
+
+class OffersBot:
+
+    def exit_handler(signal_received, frame):
+        logging.info(f"[*] Signal received ({signal_received})....Exiting.")
+        exit()
+
+    def __init__(self, channel_name, bot_token):
+
+        self.channel_name = channel_name
+        self.token = bot_token
+
+        # Set up Discord
+        logging.info("[*] Initializing Discord bot...")
+        self.discord_bot = discord.AutoShardedClient()
+        self.bot_events()
+        logging.info("[+] Done initializing Discord bot.")
+        logging.info("[+] Exiting __init__ function.")
+
+        logging.info("[*] Setting up signal handlers")
+        signal(SIGINT, self.exit_handler)
+        signal(SIGTERM, self.exit_handler)
+
+    def bot_events(self):
+
+        @self.discord_bot.event
+        async def on_ready():
+
+            logging.info("[+] Bot on_ready. Connected to Discord")
+            logging.info("[*] Name: {}".format(self.discord_bot.user.name))
+            logging.info("[*] ID: {}".format(self.discord_bot.user.id))
+
+        @self.discord_bot.event
+        async def on_message():
+            pass
